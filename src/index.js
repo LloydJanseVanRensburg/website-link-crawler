11684174d64eaeb2b0b4b975aa056d74e71d23ce
LloydJanseVanRensburg <thefullstackjunkie@gmail.com>
2023-09-03 20:42:20 +0200

    work done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
 import router from "./routes/index.js";
 import fileDirName from "./utils/dirname.js";
 import path from "path";
 
-const { __dirname } = fileDirName(import.meta)
-const publicDir = path.join(__dirname, 'public');
+const { __dirname } = fileDirName(import.meta);
+const publicDir = path.join(__dirname, "public");
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static(publicDir))
-app.use('/', router);
+app.use(morgan("dev"));
+app.use(express.static(publicDir));
+app.use("/", router);
 
-app.listen('3000', () => {
-    console.log('Server running on port 3000')
-})
+app.listen("3000", () => {
+  console.log("Server running on port 3000");
+});
