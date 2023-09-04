f2f4e1cf49d545aa8a2edfc7d23db13d95fd87b5
LloydJanseVanRensburg <thefullstackjunkie@gmail.com>
2023-09-04 16:05:57 +0200

    changed form .head request to .get

--- src/utils/checkLinkStatus.js
+++ src/utils/checkLinkStatus.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function checkLinkStatus(link) {
   try {
-    const response = await axios.head(link);
+    const response = await axios.get(link);
     const status = response.status;
 
     return status === 200;
